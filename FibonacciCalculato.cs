a26de1af47b574ef53a7c932ad67323cbc4d8d42
emins1856 <164320479+emins1856@users.noreply.github.com>
2024-04-25 10:23:47 +0300

    Add files via upload

---
+++ FibonacciCalculato.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class FibonacciCalculator
+{
+    private int _numberOfTerms;
+
+    public FibonacciCalculator(int numberOfTerms)
+    {
+        _numberOfTerms = numberOfTerms;
+    }
+
+    public int[] CalculateSequence()
+    {
+        if (_numberOfTerms < 1) return new int[0];
+
+        int[] sequence = new int[_numberOfTerms];
+        if (_numberOfTerms > 0) sequence[0] = 0; 
+        if (_numberOfTerms > 1) sequence[1] = 1; 
+
+        for (int i = 2; i < _numberOfTerms; i++)
+        {
+            sequence[i] = sequence[i - 1] + sequence[i - 2];
+        }
+        return sequence;
+    }
+
+    public (int max, int iterations) GetStatistics()
+    {
+        int[] sequence = CalculateSequence();
+        return (sequence.Length > 1 ? sequence[^1] : 0, sequence.Length - 1);
+    }
+
+    public static (int[] numbers, int[] maxValues, int[] iterations) CalculateRange(int start, int end)
+    {
+        int rangeSize = end - start + 1;
+        int[] numbers = new int[rangeSize];
+        int[] maxValues = new int[rangeSize];
+        int[] iterations = new int[rangeSize];
+
+        for (int i = 0; i < rangeSize; i++)
+        {
+            int currentNumber = start + i;
+            FibonacciCalculator calculator = new FibonacciCalculator(currentNumber);
+            var stats = calculator.GetStatistics();
+            numbers[i] = currentNumber;
+            maxValues[i] = stats.max;
+            iterations[i] = stats.iterations;
+        }
+        return (numbers, maxValues, iterations);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of elements to print in the Fibonacci sequence: ");
+        int terms = Convert.ToInt32(Console.ReadLine());
+
+        FibonacciCalculator calculator = new FibonacciCalculator(terms);
+        int[] sequence = calculator.CalculateSequence();
+        Console.Write("Fibonacci Series: ");
+        foreach (int num in sequence)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+
+        var (maxValue, iterations) = calculator.GetStatistics();
+        Console.WriteLine($"Max Value: {maxValue}");
+        Console.WriteLine($"Total Iterations: {iterations}");
+
+        Console.WriteLine("\nEnter the start value for the range: ");
+        int start = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the finish value for the range: ");
+        int end = Convert.ToInt32(Console.ReadLine());
+
+        var (numbers, maxValues, iterationsArray) = FibonacciCalculator.CalculateRange(start, end);
+        Console.WriteLine($"\nResults for the range [{start}, {end}]:");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine($"Number: {numbers[i]}, Max Value: {maxValues[i]}, Iterations: {iterationsArray[i]}");
+        }
+    }
+}
